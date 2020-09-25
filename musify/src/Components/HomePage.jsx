0c14ff611cd7a171756df78b8cd5dd914e09ceb6
Khushi Patel <khushipatel81420@gmail.com>
2020-09-25 16:28:53 +0530

    ui-updated

--- musify/src/Components/HomePage.jsx
+++ musify/src/Components/HomePage.jsx
@@ -1,63 +1,130 @@
 import React, { Component } from "react";
 import "../Styles/HomePage.css";
 import TrackCard from "./TrackCard";
-import { useEffect, useState } from 'react'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons/faSearch";
-import { faPlay } from "@fortawesome/free-solid-svg-icons/faPlay";
+import { useEffect, useState } from "react";
 import "../Images/bg.svg";
-import Footer from "./Footer";
 import GetPlaylist from "../api/GetPlaylist";
 const HomePage = () => {
-  const [result, setResult] = useState()
-  const [tracksrc, setTrackSrc] = useState()
+  const [result, setResult] = useState();
+  const [tracksrc, setTrackSrc] = useState();
   const onSearchPressed = async () => {
-    const inputMood = document.getElementById('inputMood').value;
+    const inputMood = document.getElementById("inputMood").value;
     setResult(await GetPlaylist(inputMood));
-  }
+  };
   const loadPlayer = () => {
-    let player = document.getElementById('player');
+    let player = document.getElementById("player");
     player.load();
     player.play();
-  }
-    return (
-      <div className="">
-        <nav class="navbar navbar-expand-lg " style={{ backgroundColor: '#90a4ae' }}><a class="navbar-brand text-light" href="#">Musify</a></nav>
+  };
+  return (
+    <div className="page">
+      <nav class="navbar navbar-expand-lg ">
+        <a class="navbar-brand text-light" href="#">
+          Musify
+        </a>
+      </nav>
+      <div className="bg">
         <div className="container">
           <div class="input-group mb-3 pt-4">
-            <input id="inputMood" type="text" class="form-control" placeholder="What's your mood?" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-              <div class="input-group-append">
-                <button class="btn btn-outline-secondary" type="button" id="button-addon2" onClick={onSearchPressed}>Search</button>
-              </div>
+            <input
+              id="inputMood"
+              type="text"
+              class="form-control"
+              placeholder="What's your mood?"
+              aria-label="Recipient's username"
+              aria-describedby="button-addon2"
+            />
+            <div class="input-group-append">
+              <button
+                class="btn btn-outline-secondary"
+                type="button"
+                id="button-addon2"
+                onClick={onSearchPressed}
+              >
+                Search
+              </button>
+            </div>
           </div>
-          <h5 className="float-left">Quick Picks: </h5>
-          <h4><span class="badge badge-pill badge-light float-left">Cheerful</span></h4>
-          <h4><span class="badge badge-pill badge-light float-left">Nostalgic</span></h4>
-          <h4><span class="badge badge-pill badge-light float-left">Funky</span></h4>
-          <h4><span class="badge badge-pill badge-light float-left">Cool</span></h4>
-          <h4><span class="badge badge-pill badge-light float-left">Lightmooded</span></h4>
+          <h5 className="float-left text-color">Quick Picks: </h5>
+          <h4>
+            <span class="badge badge-pill badge-light float-left">
+              Cheerful
+            </span>
+          </h4>
+          <h4>
+            <span class="badge badge-pill badge-light float-left">
+              Nostalgic
+            </span>
+          </h4>
+          <h4>
+            <span class="badge badge-pill badge-light float-left">Funky</span>
+          </h4>
+          <h4>
+            <span class="badge badge-pill badge-light float-left">Cool</span>
+          </h4>
+          <h4>
+            <span class="badge badge-pill badge-light float-left">
+              Lightmooded
+            </span>
+          </h4>
         </div>
-        <br/>
+        <br />
         <hr />
-        
+
         <div className="playlist p-4">
-          
-          {result && result ? <h4 className="text-left">Our Playlist Suggestion</h4> :<h4 className="text-left">Recommended</h4> }
-          {result && result ? result.map((x, key) => <TrackCard setActiveTrack={(name)=>{setTrackSrc(name);loadPlayer()}} Id={++key} Name={x.Track_Name} Artist={x.Track_Artist} Mood={x.Track_Mood} Duration={x.Track_Duration}/> ) : '' }
-         
+          {result && result ? (
+            <h4 className="text-left text-color">Our Playlist Suggestion</h4>
+          ) : (
+            <h4 className="text-left ">Recommended</h4>
+          )}
+          {result && result
+            ? result.map((x, key) => (
+                <TrackCard
+                  mt-2
+                  mb-2card
+                  mt-2
+                  mb-2rd
+                  setActiveTrack={(name) => {
+                    setTrackSrc(name);
+                    loadPlayer();
+                  }}
+                  Id={++key}
+                  Name={x.Track_Name}
+                  Artist={x.Track_Artist}
+                  Mood={x.Track_Mood}
+                  Duration={x.Track_Duration}
+                />
+              ))
+            : ""}
         </div>
 
-        <nav class="navbar navbar-expand-lg " style={{ backgroundColor: '#191919', position: 'fixed', bottom: 0, width: '100%' }}>
-          <h5 className="text-light">{tracksrc?tracksrc:''}</h5>
-          <audio id='player' classname="float-right" controls style={{ backgroundColor: 'rgba(0, 0, 0, 0)', width: '100%'}}>
-            <source src="horse.ogg" type="audio/ogg"/>
-            <source src={'https://creedorians.000webhostapp.com/' + tracksrc + '.mp3'} type="audio/mpeg"/>
-                Your browser does not support the audio element.
+        <nav
+          class="navbar navbar-expand-lg "
+          style={{
+            backgroundColor: "#191919",
+            position: "fixed",
+            bottom: 0,
+            width: "100%",
+          }}
+        >
+          <h5 className="text-light">{tracksrc ? tracksrc : ""}</h5>
+          <audio
+            id="player"
+            classname="float-right"
+            controls
+            style={{ width: "100%" }}
+          >
+            <source src="horse.ogg" type="audio/ogg" />
+            <source
+              src={"https://creedorians.000webhostapp.com/" + tracksrc + ".mp3"}
+              type="audio/mpeg"
+            />
+            Your browser does not support the audio element.
           </audio>
         </nav>
-
       </div>
-    );
-  }
+    </div>
+  );
+};
 
 export default HomePage;
--- musify/src/Components/TrackCard.jsx
+++ musify/src/Components/TrackCard.jsx
@@ -1,29 +1,23 @@
-import React from 'react';
+import React from "react";
+import "../Styles/TrackCard.css";
 const TrackCard = (props) => {
-    return (
-        <div class="card mt-2 mb-2" onClick={()=>props.setActiveTrack(props.Name)}>
-            <div class="card-body">
-                <div class="container-fluid">
-                    <div className="row">
-                        <div class="col-sm-1">
-                            {props.Id}
-                        </div>
-                        <div class="col-sm-4">
-                            {props.Name}
-                        </div>
-                        <div class="col-sm-4">
-                            {props.Artist}
-                        </div>
-                        <div class="col-sm-2">
-                            {props.Mood}
-                        </div>
-                        <div class="col-sm-1">
-                            {props.Duration}
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div
+      class="card mt-2 mb-2"
+      onClick={() => props.setActiveTrack(props.Name)}
+    >
+      <div class="card-body">
+        <div class="container-fluid">
+          <div className="row">
+            <div class="col-sm-1">{props.Id}</div>
+            <div class="col-sm-4">{props.Name}</div>
+            <div class="col-sm-4">{props.Artist}</div>
+            <div class="col-sm-2">{props.Mood}</div>
+            <div class="col-sm-1">{props.Duration}</div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 export default TrackCard;
